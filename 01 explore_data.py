323112d6401474aff0ce527bed7173608c2222df
EthicallyPython <97242199+EthicallyPython@users.noreply.github.com>
2023-02-24 13:27:15 -0500

    Create 01 explore_data.py

---
+++ 01 explore_data.py
@@ -0,0 +1,33 @@
+# required libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+## prettifies seaborn
+sns.set_style('whitegrid')
+
+## if using Jupyter Notebook:
+#%matplotlib inline
+
+#################################################
+
+# data cleaning
+"""
+drop outliers and irrelevant data here
+"""
+## read csv file
+df = pd.read_csv('file.csv')
+
+## Visualize data
+print(df.head())
+print(df.describe())
+print(df.info())
+print(df.columns)
+
+### Exploring data (aka Exploratory Data Analysis)
+sns.heatmap(df.corr(), annot=True)
+sns.pairplot(df) # if dataset is big, comment this line out
+sns.histplot(df['column'])
+sns.countplot(df['column'])
+sns.scatterplot(x='', y='', data=df)
