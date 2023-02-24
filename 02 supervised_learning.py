d0e6895edacca2fec3179d205f6f97c75d3d9f11
EthicallyPython <97242199+EthicallyPython@users.noreply.github.com>
2023-02-24 15:05:54 -0500

    Remove model evaluation part

--- 02 supervised_learning.py
+++ 02 supervised_learning.py
@@ -15,29 +15,3 @@ model = Model()
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 
-# EVALUATE MACHINE PERFORMANCE
-## classification: seeing how well model did
-from sklearn.metrics import classification_report, ConfusionMatrixDisplay
-print(classification_report(y_test, predictions))
-print(ConfusionMatrixDisplay.from_predictions(y_test, predictions))
-
-### flipping predicted and true for clarity
-cm = confusion_matrix(y_test, predictions)
-cmp = ConfusionMatrixDisplay(cm, display_labels=['label_1', 'label_2'])
-
-plt.xlabel('Actual')
-plt.ylabel('Predicted')
-
-### remove grid lines for confusion matrix
-plt.grid(False)
-plt.show()
-
-## regression: mean errors. Checks how close estimates are to actual values
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-print(mean_absolute_error(y_test, predictions))
-print(mean_squared_error(y_test, predictions))
-### RMSE (root mean squared error)
-print(np.sqrt(mean_squared_error(y_test, predictions)))
-
-## show relations between column and number. 
-coeff = pd.DataFrame(model.coef_, X.columns, columns=['Coeff'])
