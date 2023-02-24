0d76bd3e64ed7f596cbd4b35e10c7b4d11511b8b
EthicallyPython <97242199+EthicallyPython@users.noreply.github.com>
2023-02-24 13:41:35 -0500

    Fix MinMaxScaler import

--- 02 deep_learning.py
+++ 02 deep_learning.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 ## scale data. ALL DATA MUST BE SCALED
-from sklearn.preprocessing import MinMaxScaler()
+from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler() #replace with any scaler
   
 X_train = scaler.fit_transform(X_train)
