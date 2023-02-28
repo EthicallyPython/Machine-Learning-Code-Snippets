0861d75a74d6521da844aa15ddf950bae36314fc
EthicallyPython <97242199+EthicallyPython@users.noreply.github.com>
2023-02-28 14:24:13 -0500

    Update 03 evaluate_model.py

--- 03 evaluate_model.py
+++ 03 evaluate_model.py
@@ -3,14 +3,7 @@
 from sklearn.metrics import classification_report, ConfusionMatrixDisplay, confusion_matrix
 
 print(classification_report(y_test, predictions))
-print(ConfusionMatrixDisplay.from_predictions(y_test, predictions))
-
-### flipping predicted and true for clarity
-cm = confusion_matrix(y_test, predictions)
-cmp = ConfusionMatrixDisplay(cm, display_labels=['label_1', 'label_2'])
-
-### removes lines if you're using seaborn
-sns.reset_orig()
+print(ConfusionMatrixDisplay.from_predictions(y_test, predictions, display_labels=[False, True]))
 
 ## REGRESSION
 from sklearn.metrics import mean_absolute_error, mean_squared_error
