4abaa3c37bccf27f2390f6fe58e0cb2ba000f1f4
EthicallyPython <97242199+EthicallyPython@users.noreply.github.com>
2023-03-02 14:20:51 -0500

    Fix formatting

--- 02 deep_learning.py
+++ 02 deep_learning.py
@@ -65,7 +65,7 @@ early_stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1,
 ## fit model to data
 model.fit(x=X_train,
 					y=y_train,
-          epochs=250,
+					epochs=250,
           validation_data=(X_test, y_test),
           batch_size=128, # smaller = longer, but less likely to overfit
           callbacks = [early_stop]
