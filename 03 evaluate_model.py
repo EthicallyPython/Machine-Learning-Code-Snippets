24f83787fcb427a898cc43b10be8e548dc788f0c
EthicallyPython <97242199+EthicallyPython@users.noreply.github.com>
2023-03-02 14:31:37 -0500

    Add deep learning predictions adjuster

--- 03 evaluate_model.py
+++ 03 evaluate_model.py
@@ -2,8 +2,13 @@
 ## CLASSIFICATION
 from sklearn.metrics import classification_report, ConfusionMatrixDisplay, confusion_matrix
 
+### Deep Learning: uncomment below if using deep learning
+#predictions = [np.round(prediction) for prediction in predictions]
+
 print(classification_report(y_test, predictions))
 print(ConfusionMatrixDisplay.from_predictions(y_test, predictions, display_labels=[False, True]))
+
+
 
 ## REGRESSION
 from sklearn.metrics import mean_absolute_error, mean_squared_error
@@ -16,4 +21,3 @@ print(np.sqrt(mean_squared_error(y_test, predictions)))
 ## show relations between column and number. For logistic and linear regression only
 coef = pd.DataFrame(model.coef_, X.columns, columns=['Coeff'])
 
-## DEEP LEARNING: see 02 deep_learning.py
