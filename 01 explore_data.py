3df45165ff49465a7e7fa7803d2ed0fe1509b6e8
EthicallyPython <97242199+EthicallyPython@users.noreply.github.com>
2023-03-02 15:01:24 -0500

    Add undersampling

--- 01 explore_data.py
+++ 01 explore_data.py
@@ -16,6 +16,24 @@ sns.set_style('whitegrid')
 """
 drop outliers and irrelevant data here
 """
+
+"""
+UNDERSAMPLING: balances uneven datasets by keeping all of the data in the minority class and selecting randomly from majority class
+column_to_predict = ''
+
+majority = df[df[column_to_predict] == 0]
+minority = df[df[column_to_predict] == 1]
+
+# grab random samples (as many as there are in minority class)
+undersample = []
+for i in minority['age'].values:
+    undersample.append(majority.iloc[random.randint(0, len(majority))])
+
+undersample = pd.DataFrame(undersample)
+
+undersampled_data = pd.concat([minority, undersample])
+"""
+
 ## read csv file
 df = pd.read_csv('file.csv')
 
