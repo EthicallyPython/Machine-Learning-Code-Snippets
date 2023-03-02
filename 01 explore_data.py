1245e84397a6d97bd6b6bb4e546071062538e00e
EthicallyPython <97242199+EthicallyPython@users.noreply.github.com>
2023-03-02 14:07:28 -0500

    Move one hot encoding before data visualization

--- 01 explore_data.py
+++ 01 explore_data.py
@@ -19,19 +19,6 @@ drop outliers and irrelevant data here
 ## read csv file
 df = pd.read_csv('file.csv')
 
-## Visualize data
-print(df.head())
-print(df.describe())
-print(df.info())
-print(df.columns)
-
-### Exploring data (aka Exploratory Data Analysis)
-sns.heatmap(df.corr(), annot=True)
-sns.pairplot(df) # if dataset is big, comment this line out
-sns.histplot(df['column'])
-sns.countplot(df['column'])
-sns.scatterplot(x='', y='', data=df)
-
 ## one-hot encoding. Look for invalid data. Turn them into numbers
 invalid_col = df.select_dtypes(['object']).columns
 dummies = pd.get_dummies(df[invalid_col])
@@ -45,3 +32,16 @@ dummies.drop(col_to_drop, axis=1, inplace=True)
 
 ### add encoded columns. Drop invalid columns from df
 df_new = pd.concat([df.drop(invalid_col, axis=1), dummies], axis=1)
+
+## Visualize data
+print(df.head())
+print(df.describe())
+print(df.info())
+print(df.columns)
+
+### Exploring data (aka Exploratory Data Analysis)
+sns.heatmap(df.corr(), annot=True)
+sns.pairplot(df) # if dataset is big, comment this line out
+sns.histplot(df['column'])
+sns.countplot(df['column'])
+sns.scatterplot(x='', y='', data=df)
