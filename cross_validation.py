8e3858d7a2360dc713c2684f600332656c8a53bc
EthicallyPython <97242199+EthicallyPython@users.noreply.github.com>
2023-04-20 15:34:27 -0400

    Create cross_validation.py

---
+++ cross_validation.py
@@ -0,0 +1,20 @@
+# CROSS VALIDATION: split data into training and test sets
+
+## hold-out
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=155)
+
+## k-fold cross-validation: split into a certain number of groups
+from sklearn.model_selection import KFold
+kf = KFold(n_splits=10)
+kf.get_n_splits(X)
+
+X_train, X_test, y_train, y_test = [], [], [], []
+
+### NOTE: you will need to loop over train and test data when preprocessing and training model 
+for train_index, test_index in kf.split(X):
+    print("TRAIN:", train_index, "TEST:", test_index)
+    X_train.append(X[train_index])
+    X_test.append(X[test_index])
+    y_train.append(y[train_index])
+    y_test.append(y[test_index])
